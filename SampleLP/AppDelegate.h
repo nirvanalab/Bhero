a7a1bbf9e2e84fe984a51510b45f1eef8d8db372
Vidhur Voora <vidhurv@microsoft.com>
2017-07-21 19:50:08 -0700

    Initial Commit

---
+++ SampleLP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleLP
+//
+//  Created by Vidhur Voora on 7/21/17.
+//  Copyright © 2017 Vidhur Voora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleLP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleLP
+//
+//  Created by Vidhur Voora on 7/21/17.
+//  Copyright © 2017 Vidhur Voora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
